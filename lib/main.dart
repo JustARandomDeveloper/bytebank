8b9970a186e9006c9b094a1f558b015624f82bd8
Gabriel Augusto Piedade Inacio <gabrielpinacio@protonmail.com>
2021-06-14 17:11:08 -0300

    Salvando e buscando contatos

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
+import 'package:bytebank/database/app_database.dart';
+import 'package:bytebank/models/contact.dart';
+import 'package:bytebank/screens/contact_form.dart';
+import 'package:bytebank/screens/contacts_list.dart';
 import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
+import 'models/contact.dart';
+
 void main() {
   runApp(BytebankApp());
+  findAll().then((contacts) => debugPrint(contacts.toString()));
 }
 
 class BytebankApp extends StatelessWidget {
   // This widget is the root of your application.
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Colors.green[900],
         accentColor: Colors.blueAccent[700],
