3136e68aedbaa4f6b1a04138ccef03fe037afc50
Gabriel Augusto Piedade Inacio <gabrielpinacio@protonmail.com>
2021-06-20 14:37:59 -0300

    Novas funcionalidades no Dashboard

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,10 @@ class BytebankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Colors.green[900],
+        // ignore: deprecated_member_use
         accentColor: Colors.blueAccent[700],
         buttonTheme: ButtonThemeData(
           buttonColor: Colors.blueAccent[700],
