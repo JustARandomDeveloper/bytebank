60e793651bb07540a9aa8885109862380c07578f
Gabriel Augusto Piedade Inacio <gabrielpinacio@protonmail.com>
2021-06-22 17:26:31 -0300

    http p2

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -4,33 +4,21 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:http/http.dart';
+import 'package:http_interceptor/http_interceptor.dart';
 
-// void findAll() async{
-//   final Response response = await get(Uri.http('http://192.168.68.114:8080', 'transactions'));//192.168.68.114:8080/transactions');
-//   print(response.body);
-// }
-  
-// Future<Post> findthishit() async {
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({RequestData data}) async {
+    print(data);
+    return data;
+  }
 
-//   final response = await http.get('http://192.168.68.114:8080/transactions');
-//   // return http.get(Uri.parse('http://192.168.68.114:8080/transactions'));
-//   // print(response);
-//   if (response.statusCode == 200){
-//     return Post.fromJson(json.decode(response.body));
-//   } else{
-//     throw Exception("Failed to load the post, try again later");
-//   }
-// }
-
-// class Post{
-//   final int userId;
-//   final int id;
-//   final String title;
-//   final String body;
-  
-//   Post({this.userId, this.id, this.title, this.body});
-
-// }
+  @override
+  Future<ResponseData> interceptResponse({ResponseData data}) async {
+      print(data);
+      return data;
+  }
+}
 
 void findthishit() async{
   final url = 'http://192.168.68.114:8080/transactions';
