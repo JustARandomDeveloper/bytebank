759053d4ea10b2b4ae8e66393ad701b7d0ac3d02
Gabriel Augusto Piedade Inacio <gabrielpinacio@protonmail.com>
2021-06-25 16:10:54 -0300

    Utilizando o interceptador do http

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -1,4 +1,3 @@
-
 import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
@@ -9,21 +8,59 @@ import 'package:http_interceptor/http_interceptor.dart';
 class LoggingInterceptor implements InterceptorContract {
   @override
   Future<RequestData> interceptRequest({RequestData data}) async {
-    print(data);
+    print(data.toString());
     return data;
   }
 
   @override
   Future<ResponseData> interceptResponse({ResponseData data}) async {
-      print(data);
-      return data;
+    print(data.toString());
+    return data;
   }
 }
 
-void findthishit() async{
+class WeatherApiInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({RequestData data}) async {
+    try {
+      // data.params['appid'] = OPEN_WEATHER_API_KEY;
+      data.params['units'] = 'metric';
+      data.headers["Content-Type"] = "application/json";
+    } catch (e) {
+      print(e);
+    }
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({ResponseData data}) async => data;
+}
+
+
+void findthishit() async {
   final url = 'http://192.168.68.114:8080/transactions';
   final response = await get(Uri.parse(url));
   final jsonData = jsonDecode(response.body) as List;
   print(jsonData);
+  
+  Client client = InterceptedClient.build(interceptors:[
+    WeatherApiInterceptor(),
+  ]);
+  Future<Map<String, dynamic>> fetchCityWeatherK(int id) async{
+    var parsedWeather;
+    try {
+      // final response =
+      //     await client.get("url/weather".toUri(), params: {'id': "$id"});
+      if (response.statusCode == 200) {
+        parsedWeather = json.decode(response.body);
+      } else {
+        throw Exception("Error while fetching. \n ${response.body}");
+      }
+    } catch (e) {
+      print(e);
+    }
+    return parsedWeather;
+  }
+  
+  
 }
-
